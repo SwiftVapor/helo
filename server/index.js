fae4de5ca1ed7f3aacc03b2a23212e5ec16e6a77
SwiftVapor <jonathongthayne@gmail.com>
2020-10-09 11:28:46 -0600

    Have the server workign 4 real this time

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const express = require ('express'),
     session = require ('express-session'),
     ctrl = require ('./controller'),
     {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env,
-    port = 4000,
+    port = SERVER_PORT,
     app = express();
 
     app.use (express.json())
